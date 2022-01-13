eaa0bcbf057229fefcdfcb27706fee1598d2106a
munib <info@smart387.ai>
2022-01-13 13:48:39 +0100

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
-import Layout from '../ui/Layout';
+import Layout from "../components/ui/Layout";
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-    </Layout>);
+    </Layout>
+  );
 }
 
 export default MyApp;
