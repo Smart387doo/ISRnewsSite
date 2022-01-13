ff9f76a2b76093d4853efbeebe89ea0013b7c80e
munib <info@smart387.ai>
2022-01-13 13:37:39 +0100

    commit

---
+++ ui/Layout.js
@@ -0,0 +1,5 @@
+const Layout = ({ children }) => {
+  return <div className="mx-auto max-w-7xl">{children}</div>;
+};
+
+export default Layout;
--- ui/Layout.jsx
+++
@@ -1,5 +0,0 @@
-const Layout = ({ children }) => {
-  return <main className="mx-auto max-w-7xl">{children}</main>;
-};
-
-export default Layout;
