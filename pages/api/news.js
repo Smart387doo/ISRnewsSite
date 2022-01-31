c98d8d73a7f6f35db10939d40552234781284c85
munib <info@smart387.ai>
2022-01-31 19:48:21 +0100

    commit

--- pages/api/news.js
+++ pages/api/news.js
@@ -1,7 +1,7 @@
-import { getKlixNews, getN1News, getOslobodjenjeNews, getVecernjiListNews } from "../../helper/helperFunctions";
+import { getKlixNews1, getN1News, getOslobodjenjeNews, getVecernjiListNews } from "../../helper/helperFunctions";
 export default async function handler(req, res) {
   const n1News = await getN1News();
-  const klixNews = await getKlixNews();
+  const klixNews = await getKlixNews1();
   const vecernjiNews = await getVecernjiListNews();
   const oslobodjenjeNews = await getOslobodjenjeNews();
   res.status(200).json({ n1: n1News, klix: klixNews, vecernji: vecernjiNews, oslobodjenje: oslobodjenjeNews });
