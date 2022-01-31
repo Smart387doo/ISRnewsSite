9aaf8651db7c95d05fd2ac1b7f15d9c21f07efab
munib <info@smart387.ai>
2022-01-31 19:34:45 +0100

    commit

--- helper/helperFunctions.js
+++ helper/helperFunctions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const fetch_retry = async (url, n) => {
   try {
-    return await fetch(url, { mode: 'no-cors' })
+    return await fetch(url, { mode: 'cors' })
       .then(response => response.text())
       .then(data => { return data; });
   } catch (err) {
