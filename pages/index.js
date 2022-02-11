7d984f7d6237c84ef822e062260b3643b02fdc19
munib <info@smart387.ai>
2022-02-11 13:57:43 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home(props) {
       <h1 className="p-4 my-4 text-3xl font-semibold text-center underline rounded-md shadow bg-slate-100 decoration-rose-700">
         N1 info
       </h1>
-      <div className="grid mt-6 space-y-12 lg:space-y-0 lg:grid-cols-5 lg:gap-6 md:grid-cols-2">
+      <div className="grid mt-6 mb-16 space-y-12 lg:space-y-0 lg:grid-cols-5 lg:gap-6 md:grid-cols-2">
         {n1.map(news => <NewsCard key={news.title} news={news} />)}
       </div>
 
